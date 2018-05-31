5d5be002159aba6a124119ea6f0d3b75e1c0f8f7
Modou Lo <modoulo99@icloud.com>
2018-05-31 14:55:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
@@ -16,6 +17,26 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element);
   return array;
+=======
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+>>>>>>> f996e09e097fb8b893e5a4ddb574a3fc5bb6c5a3
 }
 
 function accessElementInArray(array, index) {
@@ -23,8 +44,13 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+<<<<<<< HEAD
   array.shift();
   return array;
+=======
+  array.shift()
+  return array
+>>>>>>> f996e09e097fb8b893e5a4ddb574a3fc5bb6c5a3
 }
 
 function removeElementFromBeginningOfArray(array) {
